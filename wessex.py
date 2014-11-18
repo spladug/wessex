e85ab52335908181c62204f8e63c3b725af30920
Neil Williams <neil@reddit.com>
2014-11-18 08:50:36 -0800

    Make harold's URL more configurable.

--- wessex.py
+++ wessex.py
@@ -1,3 +1,4 @@
+import posixpath
 import urllib
 import urllib2
 import urlparse
@@ -7,37 +8,41 @@ __version__ = '1.5.0'
 
 
 class Harold(object):
-    def __init__(self, host, secret, port=80, timeout=3):
-        self.host = host
-        self.port = port
+    def __init__(self, url, secret, timeout=3):
+        self.scheme, self.netloc, self.path, query, fragment = urlparse.urlsplit(url)
+        assert not query, "harold url may not contain query parameters."
+        assert not fragment, "harold url may not contain fragments."
+
         self.secret = secret
         self.timeout = timeout
 
     def _post_to_harold(self, path, data):
-        path_components = ['harold'] + path + [self.secret]
-        combined_path = '/'.join(path_components)
-        netloc = "%s:%d" % (self.host, self.port)
-        url = urlparse.urlunsplit(("http", netloc, combined_path, None, None))
-        encoded_data = urllib.urlencode(data)
-        urllib2.urlopen(url, data=encoded_data, timeout=self.timeout)
+        combined_path = posixpath.join(self.path, "harold", path, self.secret)
+        url = urlparse.urlunsplit((
+            self.scheme,
+            self.netloc,
+            combined_path,
+            None,
+            None,
+        ))
+
+        urllib2.urlopen(
+            url,
+            data=urllib.urlencode(data),
+            timeout=self.timeout,
+        )
 
     def alert(self, tag, message):
-        self._post_to_harold(
-            ["alert"],
-            {
-                "tag": tag,
-                "message": message,
-            }
-        )
+        self._post_to_harold("alert", {
+            "tag": tag,
+            "message": message,
+        })
 
     def heartbeat(self, tag, interval):
-        self._post_to_harold(
-            ["heartbeat"],
-            {
-                "tag": tag,
-                "interval": interval,
-            }
-        )
+        self._post_to_harold("heartbeat", {
+            "tag": tag,
+            "interval": interval,
+        })
 
     def get_irc_channel(self, channel):
         return IrcChannel(self, channel)
@@ -52,22 +57,16 @@ class IrcChannel(object):
         self.channel = channel
 
     def message(self, message):
-        self.harold._post_to_harold(
-            ["message"],
-            {
-                "channel": self.channel,
-                "message": message,
-            }
-        )
+        self.harold._post_to_harold("message", {
+            "channel": self.channel,
+            "message": message,
+        })
 
     def set_topic(self, topic):
-        self.harold._post_to_harold(
-            ["topic", "set"],
-            {
-                "channel": self.channel,
-                "topic": topic,
-            }
-        )
+        self.harold._post_to_harold("topic/set", {
+            "channel": self.channel,
+            "topic": topic,
+        })
 
 
 class Deploy(object):
@@ -76,52 +75,37 @@ class Deploy(object):
         self.id = id
 
     def begin(self, who, args, log_path, host_count):
-        self.harold._post_to_harold(
-            ["deploy", "begin"],
-            {
-                "id": self.id.encode('utf-8'),
-                "who": who,
-                "args": args,
-                "log_path": log_path.encode('utf-8'),
-                "count": host_count
-            }
-        )
+        self.harold._post_to_harold("deploy/begin", {
+            "id": self.id.encode('utf-8'),
+            "who": who,
+            "args": args,
+            "log_path": log_path.encode('utf-8'),
+            "count": host_count
+        })
 
     def end(self):
-        self.harold._post_to_harold(
-            ["deploy", "end"],
-            {
-                "id": self.id.encode('utf-8'),
-            }
-        )
+        self.harold._post_to_harold("deploy/end", {
+            "id": self.id.encode('utf-8'),
+        })
 
     def error(self, error):
-        self.harold._post_to_harold(
-            ["deploy", "error"],
-            {
-                "id": self.id.encode('utf-8'),
-                "error": error,
-            }
-        )
+        self.harold._post_to_harold("deploy/error", {
+            "id": self.id.encode('utf-8'),
+            "error": error,
+        })
 
     def abort(self, reason):
-        self.harold._post_to_harold(
-            ["deploy", "abort"],
-            {
-                "id": self.id.encode('utf-8'),
-                "reason": reason,
-            }
-        )
+        self.harold._post_to_harold("deploy/abort", {
+            "id": self.id.encode('utf-8'),
+            "reason": reason,
+        })
 
     def progress(self, host, index):
-        self.harold._post_to_harold(
-            ["deploy", "progress"],
-            {
-                "id": self.id.encode('utf-8'),
-                "host": host,
-                "index": index,
-            }
-        )
+        self.harold._post_to_harold("deploy/progress", {
+            "id": self.id.encode('utf-8'),
+            "host": host,
+            "index": index,
+        })
 
 
 def connect_harold(config="/etc/harold.ini"):
@@ -130,19 +114,17 @@ def connect_harold(config="/etc/harold.ini"):
     import ConfigParser
 
     parser = ConfigParser.RawConfigParser({
-        "port": 8888,
         "timeout": 3,
     })
     files_read = parser.read(config)
     if not files_read:
         raise IOError("No config file found in: %r" % config)
 
-    host = parser.get("harold", "host")
+    url = parser.get("harold", "url")
     secret = parser.get("harold", "secret")
-    port = parser.getint("harold", "port")
     timeout = parser.getint("harold", "timeout")
 
-    return Harold(host, secret, port, timeout)
+    return Harold(url, secret, timeout)
 
 
 def harold_irc():
